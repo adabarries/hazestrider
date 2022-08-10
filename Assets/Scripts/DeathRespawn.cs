aef00eddac61bc7c35b5243d19148345bd4f7ced
Ada <adabarries@gmail.com>
2022-08-09 17:30:26 -0700

    added basic death and respawn logic

---
+++ Assets/Scripts/DeathRespawn.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathRespawn : MonoBehaviour
+{
+    public Transform spawnPoint;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Bad")
+        {
+            transform.position = spawnPoint.position;
+            // refine this and add extras later
+        } 
+    }
+}
